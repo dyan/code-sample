d6c3f17c51082317f61018f71e8d0b106b46475e
dyan <dyan@me.com>
2023-08-30 20:00:57 -0600

    moved some files for better organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatToolbarModule, MatIconModule, MatCardModule, MatButtonModule, MatProgressSpinnerModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
     AppRoutingModule, 
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    // MatToolbarModule,
+    // MatIconModule,
+    // MatButtonModule,
+    // MatCardModule,
+    // MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
